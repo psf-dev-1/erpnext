8e45b8c5d6afa55401981db5b2ecf445b715e75d
unknown <projectserverf@gmail.com>
2026-04-11 19:50:29 +0530

    Little modifications for internal branding

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
     </a>
     <h2>ERPNext</h2>
     <p align="center">
-        <p>Powerful, Intuitive and Open-Source ERP</p>
+        <p>Powerfull, Intuitive and Open-Source ERP</p>
     </p>
 
 [![Learn on Frappe School](https://img.shields.io/badge/Frappe%20School-Learn%20ERPNext-blue?style=flat-square)](https://frappe.school)<br><br>
--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -415,7 +415,7 @@ naming_series_variables = {
 	for variable in naming_series_variables_list
 }
 
-# On cancel event Payment Entry will be exempted and all linked submittable doctype will get cancelled.
+# On cancel event Payment Entry will be exempted and all linked submittable doctype will get cancelled..
 # to maintain data integrity we exempted payment entry. it will un-link when sales invoice get cancelled.
 # if payment entry not in auto cancel exempted doctypes it will cancel payment entry.
 auto_cancel_exempted_doctypes = [
